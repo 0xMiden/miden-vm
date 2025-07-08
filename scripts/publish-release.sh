9c36b6c29c7869ce80c5d63cb6a3bf6b8e722bd6
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2025-07-08 17:35:07 -0400

    fix: ensure miden-debug-types is published

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -10,6 +10,7 @@
 # to merge the fix into `next`, and then merge `next` into `main` again.
 
 cargo publish -p miden-utils-sync
+cargo publish -p miden-debug-types
 cargo publish -p miden-utils-diagnostics
 cargo publish -p miden-core
 cargo publish -p miden-air
