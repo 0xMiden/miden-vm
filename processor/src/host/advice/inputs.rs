8fe3945dab2cca430d97f498d20db1eba33a93be
Adrian Hamelink <adrian.hamelink@gmail.com>
2025-07-18 11:02:39 +0200

    Remove `AdviceInputs` visibility duplication.

--- processor/src/host/advice/inputs.rs
+++ processor/src/host/advice/inputs.rs
@@ -21,7 +21,6 @@ use miden_core::{
 /// 2. Key-mapped element lists which can be pushed onto the advice stack.
 /// 3. Merkle store, which is used to provide nondeterministic inputs for instructions that operates
 ///    with Merkle trees.
-#[cfg(not(feature = "testing"))]
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct AdviceInputs {
     pub(super) stack: Vec<Felt>,
@@ -141,17 +140,6 @@ impl Deserializable for AdviceInputs {
         let store = MerkleStore::read_from(source)?;
         Ok(Self { stack, map, store })
     }
-}
-
-// TESTING
-// ================================================================================================
-
-#[cfg(feature = "testing")]
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
-pub struct AdviceInputs {
-    pub stack: Vec<Felt>,
-    pub map: AdviceMap,
-    pub store: MerkleStore,
 }
 
 // TESTS
