e924adf003d485546217750f9a9bd6653f0c2385
Bernhard Schuster <bernhard@ahoi.io>
2025-07-29 18:59:58 +0200

    rollback source manager changes

--- crates/debug/types/src/source_manager.rs
+++ crates/debug/types/src/source_manager.rs
@@ -242,7 +242,7 @@ pub trait SourceManagerExt: SourceManager {
             .map(|s| SourceContent::new(lang, uri.clone(), s))
             .map_err(|source| {
                 SourceManagerError::custom_with_source(
-                    std::format!("failed to load filed at `{}`", path.display()),
+                    format!("failed to load filed at `{}`", path.display()),
                     source,
                 )
             })?;
@@ -254,8 +254,6 @@ pub trait SourceManagerExt: SourceManager {
 #[cfg(feature = "std")]
 impl<T: ?Sized + SourceManager> SourceManagerExt for T {}
 
-// TODO rename to reflect the fact it's a "glue" trait that adapts per target, but only after
-// the `-base` has been validated to work with this approach.
 /// [SourceManagerSync] is a marker trait for [SourceManager] implementations that are also Send +
 /// Sync, and is automatically implemented for any [SourceManager] that meets those requirements.
 ///
