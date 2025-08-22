0353681622748644758884f43d59f89235e767ef
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-08-22 15:03:10 +0300

    fix: script crashes and improve variable defaults

--- scripts/check-changelog.sh
+++ scripts/check-changelog.sh
@@ -3,17 +3,19 @@ set -u
 
 CHANGELOG_FILE="${1:-CHANGELOG.md}"
 
-if [ "${NO_CHANGELOG_LABEL}" = "true" ]; then
+if [ "${NO_CHANGELOG_LABEL:-}" = "true" ]; then
     # 'no changelog' set, so finish successfully
     echo "\"no changelog\" label has been set"
     exit 0
 else
     # a changelog check is required
     # fail if the diff is empty
-    if git diff --exit-code "origin/${BASE_REF}" -- "${CHANGELOG_FILE}"; then
-        >&2 echo "Changes should come with an entry in the \"CHANGELOG.md\" file. This behavior
-can be overridden by using the \"no changelog\" label, which is used for changes
-that are trivial / explicitely stated not to require a changelog entry."
+    if git diff --exit-code "origin/${BASE_REF:-main}" -- "${CHANGELOG_FILE}"; then
+        >&2 cat <<EOF
+Changes should come with an entry in the "CHANGELOG.md" file. This behavior
+can be overridden by using the "no changelog" label, which is used for changes
+that are trivial / explicitly stated not to require a changelog entry.
+EOF
         exit 1
     fi
 
