4c91cd8efe95da44476303a52203a9a3e4cf6ae8
Grzegorz Świrski <grzegorz@swirski.name>
2025-09-01 21:43:44 +0200

    add std::collections::sorted_array::find_word

--- stdlib/tests/collections/mod.rs
+++ stdlib/tests/collections/mod.rs
@@ -1,7 +1,8 @@
 use miden_utils_testing::{
-    EMPTY_WORD, Felt, Word, append_word_to_vec,
+    EMPTY_WORD, Felt, Word, TRUNCATE_STACK_PROC, append_word_to_vec,
     crypto::{MerkleStore, Smt},
 };
 
+mod sorted_array;
 mod mmr;
 mod smt;
---
+++ stdlib/tests/collections/sorted_array.rs
@@ -0,0 +1,26 @@
+use super::*;
+
+#[test]
+fn test_array_find_sorted_word() {
+    let source: String = format!(
+        "
+        use.std::collections::sorted_array
+
+        {TRUNCATE_STACK_PROC}
+
+        begin
+            push.2.2.2.2 mem_storew.100 dropw
+            push.3.3.3.3 mem_storew.104 dropw
+            push.4.4.4.4 mem_storew.108 dropw
+
+            push.112 push.100 push.[3,3,3,3]
+
+            exec.sorted_array::find_word
+            exec.truncate_stack
+        end
+    ");
+    let test = build_test!(source, &[]);
+    let result = test.get_last_stack_state().as_int_vec();
+    std::println!("result: {:?}", result);
+    //test.expect_stack(&[0, 0, 0, 0, 0, 0, 0]);
+}
