fc943dc2aa40f9e7fe42b32efc8b0e42c5b81095
Adrian Hamelink <adrian.hamelink@gmail.com>
2025-09-02 10:29:23 +0200

    `read_memory` requires word-alignment

--- stdlib/src/handlers/mod.rs
+++ stdlib/src/handlers/mod.rs
@@ -8,13 +8,16 @@ pub mod keccak;
 // # HELPERS
 // =================================================================================================
 
-/// Reads a contiguous region of memory from the VM process state.
+/// Reads a contiguous word-aligned region of memory from the VM process state.
 ///
 /// This function returns `None` if the provided range is invalid or if it references an
 /// uninitialized address.
 fn read_memory(process: &ProcessState, ptr: u64, len: u64) -> Option<Vec<Felt>> {
-    // Convert inputs to u32 and check for overflow
+    // Convert inputs to u32 and check for overflow + alignement.
     let start_addr: u32 = ptr.try_into().ok()?;
+    if !start_addr.is_multiple_of(4) {
+        return None;
+    }
     let len: u32 = len.try_into().ok()?;
     let end_addr = start_addr.checked_add(len)?;
 
