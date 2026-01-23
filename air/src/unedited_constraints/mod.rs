92857348bff139a28f56bad6da36b634dd53c985
Léo Besançon <lb@massa.net>
2026-01-23 16:37:17 +0100

    refactor: comment only the constraints that don't evaluate to 0

--- air/src/unedited_constraints/mod.rs
+++ air/src/unedited_constraints/mod.rs
@@ -3,7 +3,12 @@
 #[allow(unused_imports)]
 #[allow(unused_variables)]
 pub mod miden_vm_plonky3;
-#[allow(clippy::all)]
-#[allow(unused_imports)]
-#[allow(unused_variables)]
-pub mod miden_vm_plonky3_big;
+//#[allow(clippy::all)]
+//#[allow(unused_imports)]
+//#[allow(unused_variables)]
+//pub mod miden_vm_plonky3_big;
+//
+//#[allow(clippy::all)]
+//#[allow(unused_imports)]
+//#[allow(unused_variables)]
+//pub mod miden_vm_plonky3_no_crash;
