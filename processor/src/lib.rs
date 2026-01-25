15cae2b213049cf2a4f5df2495aca9a3a25bd8ca
Alex Pikme <30472093+reject-i@users.noreply.github.com>
2026-01-25 07:12:29 +0000

    update-exports

--- processor/src/lib.rs
+++ processor/src/lib.rs
@@ -33,7 +33,7 @@ use crate::{
 #[cfg(any(test, feature = "testing"))]
 mod test_utils;
 #[cfg(any(test, feature = "testing"))]
-pub use test_utils::{ProcessStateSnapshot, TestConsistencyHost, TestHost, TraceCollector};
+pub use test_utils::{ProcessStateSnapshot, TestHost, TraceCollector};
 
 #[cfg(test)]
 mod tests;
