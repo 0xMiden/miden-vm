d3e586458bd90b58b27875db6a7fc438264f344a
Léo Besançon <lb@massa.net>
2026-01-29 09:28:16 +0100

    refactor: simplify unedited_constraints to only the frame

--- air/src/unedited_constraints/mod.rs
+++ air/src/unedited_constraints/mod.rs
@@ -4,16 +4,3 @@
 #[rustfmt::skip]
 /// Only miden_vm constraints that evaluate to zero as expected
 pub mod miden_vm_plonky3;
-//#[allow(clippy::all)]
-//#[allow(unused_imports)]
-//#[allow(unused_variables)]
-//#[rustfmt::skip]
-///// All miden_vm constraints (that have already been written in .air)
-//pub mod miden_vm_plonky3_all;
-//
-//#[allow(clippy::all)]
-//#[allow(unused_imports)]
-//#[allow(unused_variables)]
-//#[rustfmt::skip]
-///// All miden_vm constraints (that have already been written in .air), except 4 RPO related constraints that cause stack-overflow
-//pub mod miden_vm_plonky3_no_crash;
