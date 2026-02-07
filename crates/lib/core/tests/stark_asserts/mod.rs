bc336c6040751ade2dd27ebe01305218d4614f62
François Garillot <francois@garillot.net>
2026-02-07 07:27:14 -0500

    Add aux randomness assert message and test

--- crates/lib/core/tests/stark_asserts/mod.rs
+++ crates/lib/core/tests/stark_asserts/mod.rs
@@ -1,4 +1,3 @@
-
 #[test]
 fn validate_inputs_trace_length_upper_bound() {
     let test = build_test!(validate_inputs_source(), &[30, 7, 0]);
@@ -40,6 +39,21 @@ fn init_seed_trace_length_too_large_has_message() {
     ";
     let test = build_test!(source, &[]);
     expect_assert_error_message!(test, contains "trace length");
+}
+
+#[test]
+fn generate_aux_randomness_mismatch_has_message() {
+    let source = "
+        use miden::core::stark::constants
+        use miden::core::stark::random_coin
+        begin
+            push.11.22.33.44 exec.constants::r1_ptr mem_storew_be dropw
+            push.99.44.11.22 exec.constants::aux_rand_nd_ptr mem_storew_be dropw
+            exec.random_coin::generate_aux_randomness
+        end
+    ";
+    let test = build_test!(source, &[]);
+    expect_assert_error_message!(test, contains "aux randomness");
 }
 
 fn validate_inputs_source() -> &'static str {
