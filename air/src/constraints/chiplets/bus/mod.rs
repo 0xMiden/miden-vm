d04cf41304f5c7e1b06774cdfe3908dbf68c258e
al <82364884+Al-Kindi-0@users.noreply.github.com>
2026-02-22 12:43:46 +0400

    constaints: kernel ROM chiplet and bus

--- air/src/constraints/chiplets/bus/mod.rs
+++ air/src/constraints/chiplets/bus/mod.rs
@@ -2,21 +2,25 @@
 //!
 //! This module groups auxiliary (bus) constraints associated with chiplets.
 //! Currently implemented:
-//! - v_wiring: ACE wiring bus
+//! - b_hash_kernel: hash-kernel virtual table bus
+//! - b_wiring: ACE wiring bus
 
+pub mod hash_kernel;
 pub mod wiring;
 
 use miden_crypto::stark::air::MidenAirBuilder;
 
-use crate::{Felt, MainTraceRow};
+use crate::{Felt, MainTraceRow, constraints::op_flags::OpFlags};
 
 /// Enforces chiplets bus constraints.
 pub fn enforce_bus<AB>(
     builder: &mut AB,
     local: &MainTraceRow<AB::Var>,
     next: &MainTraceRow<AB::Var>,
+    op_flags: &OpFlags<AB::Expr>,
 ) where
     AB: MidenAirBuilder<F = Felt>,
 {
+    hash_kernel::enforce_hash_kernel_constraint(builder, local, next, op_flags);
     wiring::enforce_wiring_bus_constraint(builder, local, next);
 }
