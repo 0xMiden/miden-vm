dfa974d95828bbdf18745a6c4bce3885bfce969c
al <82364884+Al-Kindi-0@users.noreply.github.com>
2026-04-22 01:19:23 +0400

    fix ci

--- crates/assembly-syntax/src/ast/visit.rs
+++ crates/assembly-syntax/src/ast/visit.rs
@@ -503,9 +503,9 @@ where
         | MovDn10 | MovDn11 | MovDn12 | MovDn13 | MovDn14 | MovDn15 | MovDnW2 | MovDnW3
         | Reversew | Reversedw | CSwap | CSwapW | CDrop | CDropW | PushFeltList(_) | Sdepth
         | Caller | Clk | MemLoad | MemLoadWBe | MemLoadWLe | MemStore | MemStoreWBe
-        | MemStoreWLe | MemStream | AdvPipe | AdvPush | AdvPushW | AdvLoadW
-        | Hash | HMerge | HPerm | MTreeGet | MTreeSet | MTreeMerge | MTreeVerify | FriExt2Fold4
-        | DynExec | DynCall | DebugVar(_) | HornerBase | HornerExt | CryptoStream | EvalCircuit
+        | MemStoreWLe | MemStream | AdvPipe | AdvPush | AdvPushW | AdvLoadW | Hash | HMerge
+        | HPerm | MTreeGet | MTreeSet | MTreeMerge | MTreeVerify | FriExt2Fold4 | DynExec
+        | DynCall | DebugVar(_) | HornerBase | HornerExt | CryptoStream | EvalCircuit
         | LogPrecompile | Emit => ControlFlow::Continue(()),
     }
 }
@@ -1098,9 +1098,9 @@ where
         | MovDn10 | MovDn11 | MovDn12 | MovDn13 | MovDn14 | MovDn15 | MovDnW2 | MovDnW3
         | Reversew | Reversedw | CSwap | CSwapW | CDrop | CDropW | PushFeltList(_) | Sdepth
         | Caller | Clk | MemLoad | MemLoadWBe | MemLoadWLe | MemStore | MemStoreWBe
-        | MemStoreWLe | MemStream | AdvPipe | AdvPush | AdvPushW | AdvLoadW
-        | Hash | HMerge | HPerm | MTreeGet | MTreeSet | MTreeMerge | MTreeVerify | FriExt2Fold4
-        | DynExec | DynCall | DebugVar(_) | HornerBase | HornerExt | EvalCircuit | CryptoStream
+        | MemStoreWLe | MemStream | AdvPipe | AdvPush | AdvPushW | AdvLoadW | Hash | HMerge
+        | HPerm | MTreeGet | MTreeSet | MTreeMerge | MTreeVerify | FriExt2Fold4 | DynExec
+        | DynCall | DebugVar(_) | HornerBase | HornerExt | EvalCircuit | CryptoStream
         | LogPrecompile | Emit => ControlFlow::Continue(()),
     }
 }
