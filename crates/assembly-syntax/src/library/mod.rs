1d785928a93b3263801b5d3b6e97192951408d3d
djole <djolertrk@gmail.com>
2026-04-24 18:38:11 +0200

    core: fix static linking debug metadata

--- crates/assembly-syntax/src/library/mod.rs
+++ crates/assembly-syntax/src/library/mod.rs
@@ -496,6 +496,8 @@ impl Library {
                         proc_digest,
                         signature.clone().map(Arc::new),
                         attributes.clone(),
+                        Some(*node),
+                        Some(self.mast_forest.commitment()),
                     );
                 },
                 LibraryExport::Constant(ConstantExport { path, value }) => {
@@ -675,6 +677,8 @@ impl TryFrom<Arc<Library>> for KernelLibrary {
                         proc_digest,
                         export.signature.clone().map(Arc::new),
                         export.attributes.clone(),
+                        Some(export.node),
+                        Some(library.mast_forest.commitment()),
                     );
                 },
                 LibraryExport::Constant(export) => {
