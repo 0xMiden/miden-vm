22a4237d8d643a68e6b9e11e697a25d6d5e3947c
François Garillot <francois@garillot.net>
2026-06-29 13:27:29 -0400

    precompiles: unify generated MASM codegen

--- precompiles/src/math/uint/mod.rs
+++ precompiles/src/math/uint/mod.rs
@@ -5,8 +5,6 @@ pub(crate) mod handlers;
 mod precompile;
 
 pub(crate) use self::domain::ZERO_LIMBS;
-#[cfg(feature = "codegen-tools")]
-pub(crate) use self::domain::{ONE_LIMBS, TWO_LIMBS};
 pub use self::{
     domain::{Limbs, UintDomain, UintSpec},
     precompile::UintPrecompile,
