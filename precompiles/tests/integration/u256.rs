ae4c9d385a1b7c498825a679f8353c08633d2556
François Garillot <francois@garillot.net>
2026-06-29 13:58:18 -0400

    precompiles: generate math MASM during builds

--- precompiles/tests/integration/u256.rs
+++ precompiles/tests/integration/u256.rs
@@ -3,9 +3,10 @@ use miden_precompiles::U256;
 use crate::uint_fixtures::assert_u256_contract;
 
 const MODULE: &str = "u256";
-const U256_MASM: &str = include_str!("../../asm/math/u256.masm");
 
 #[test]
 fn u256_satisfies_uint_contract() {
-    assert_u256_contract::<U256>(MODULE, U256_MASM);
+    let u256_masm = miden_precompiles_codegen::masm::render_u256_masm()
+        .expect("u256 MASM generation must succeed");
+    assert_u256_contract::<U256>(MODULE, &u256_masm);
 }
