6e5d864da40aa9c210bbaca5273618b5cc213635
François Garillot <francois@garillot.net>
2026-08-03 12:01:53 -0400

    fuzz: assert decoded package type alignments

--- tools/miden-core-fuzz/fuzz_targets/debug_info.rs
+++ tools/miden-core-fuzz/fuzz_targets/debug_info.rs
@@ -8,14 +8,56 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use miden_assembly_syntax::ast::types::Type;
 use miden_core::serde::{Deserializable, SliceReader};
 use miden_mast_package::{Package, debug_info::PackageDebugInfo};
 
+fn assert_valid_type_alignments(ty: &Type) {
+    assert!(ty.min_alignment().is_power_of_two());
+
+    match ty {
+        Type::Ptr(pointer) => assert_valid_type_alignments(pointer.pointee()),
+        Type::Struct(structure) => {
+            for field in structure.fields() {
+                assert_valid_type_alignments(&field.ty);
+            }
+        },
+        Type::Enum(enumeration) => {
+            assert_valid_type_alignments(enumeration.discriminant());
+            for variant in enumeration.variants() {
+                if let Some(value) = variant.value.as_ref() {
+                    assert_valid_type_alignments(value);
+                }
+            }
+        },
+        Type::Array(array) => assert_valid_type_alignments(array.element_type()),
+        Type::List(element) => assert_valid_type_alignments(element),
+        Type::Function(function) => {
+            for ty in function.params().iter().chain(function.results()) {
+                assert_valid_type_alignments(ty);
+            }
+        },
+        _ => {},
+    }
+}
+
+fn assert_valid_package_type_alignments(package: &Package) {
+    for procedure in package.manifest.exports().filter_map(|export| export.as_procedure()) {
+        if let Some(signature) = procedure.signature.as_ref() {
+            for ty in signature.params().iter().chain(signature.results()) {
+                assert_valid_type_alignments(ty);
+            }
+        }
+    }
+}
+
 fuzz_target!(|data: &[u8]| {
     if let Ok(package) = Package::read_from_bytes(data) {
+        assert_valid_package_type_alignments(&package);
         let _ = package.debug_info();
     }
     if let Ok(package) = Package::read_from_bytes_trusted(data) {
+        assert_valid_package_type_alignments(&package);
         let _ = package.debug_info();
     }
 
