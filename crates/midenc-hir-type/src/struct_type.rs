81479d81936837f233c239723dd65137a640adbf
François Garillot <francois@garillot.net>
2026-08-03 15:52:42 -0400

    fix: reject empty transparent structs

--- crates/midenc-hir-type/src/struct_type.rs
+++ crates/midenc-hir-type/src/struct_type.rs
@@ -211,6 +211,11 @@ impl StructType {
                         offset += field_size;
                     }
                 }
+                assert_ne!(
+                    offset, 0,
+                    "invalid transparent representation for struct: repr(transparent) requires a \
+                     non-zero sized field"
+                );
                 offset
             },
             repr => {
